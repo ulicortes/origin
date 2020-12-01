8d57e79e028d06cffcf87815f1c95a4e4b915656
ulicortes <ulisestcortes@gmail.com>
2020-12-01 01:07:09 -0300

    Preview de como quedaria la tabla, falta filter y loader

--- JS/elements.js
+++ JS/elements.js
@@ -1,17 +1,36 @@
-let postres = [
-    {postre: "Chocotorta", tiempoDePreparado: "30 minutos", cantidadDeIngredientes: 5,},
-    {postre: "Tiramisu", tiempoDePreparado: "60 minutos", cantidadDeIngredientes: 8,},
-    {postre: "Isla flotante", tiempoDePreparado: "70 minutos", cantidadDeIngredientes: 10,},
-    {postre: "Pastafrola", tiempoDePreparado: "100 minutos", cantidadDeIngredientes: 8,},
-    {postre: "Flan", tiempoDePreparado: "70 minutos", cantidadDeIngredientes: 4,},
-];
-let cabecera = ["Postre", "Tiempo de preparacion", "N° de ingredientes", " "];
-
+let desserts = `{
+        "postres": [
+            {"postre": "Chocotorta", "tiempoDePreparado": "30 minutos", "cantidadDeIngredientes": 5},
+            {"postre": "Tiramisu", "tiempoDePreparado": "60 minutos", "cantidadDeIngredientes": 8},
+            {"postre": "Isla flotante", "tiempoDePreparado": "70 minutos", "cantidadDeIngredientes": 10},
+            {"postre": "Pastafrola", "tiempoDePreparado": "100 minutos", "cantidadDeIngredientes": 8},
+            {"postre": "Flan", "tiempoDePreparado": "70 minutos", "cantidadDeIngredientes": 4}
+        ]
+    }
+`
+let dPro = JSON.parse(desserts);
+let llaves = Object.keys(dPro.postres[0]);
 let tablaEl = document.getElementById("tabla");
-let modal = document.getElementById("divModal");
-let tablaBody = document.createElement('tbody');
-let fila = document.createElement('tr');
-let thead = document.createElement('thead');
-let btn1 = document.createElement('button');
-let btn2 = document.createElement('button');
+let modalDiv = document.getElementById('div');
+let modalDivEl = document.createElement('div');
+let modalEdit = document.createElement('div');
+let elementos = dPro.postres;
 
+let carrousel = document.getElementById("carouselExampleControls");
+let modal = document.getElementById("divModal");
+
+let btnAdd = document.createElement('button');
+let btnShow = document.createElement('button');
+let agreg = document.createElement('button');
+let cancelar = document.createElement('button');
+let editar = document.createElement('button');
+
+let inpuPostre = document.createElement('input'); 
+let inpuTdp = document.createElement('input'); 
+let inpuCdi = document.createElement('input'); 
+
+let arrayInput = [];
+// console.log(postre);
+let tBody = document.createElement('tbody');
+
+let jojo = document.createElement('input');
--- JS/table.js
+++ JS/table.js
@@ -1,47 +1,263 @@
-let headeR = () => {
-    for (n in cabecera) {
-        let header = document.createElement('th');
-        header.innerText = cabecera[n];
-        header.classList.add("borders");
-        console.log(cabecera[n]);
-        thead.appendChild(header);
+let header = (llaves) => {
+    let acc = "accion";
+    llaves.push(acc)
+    let theadEl = document.createElement('thead');
+    let trEl = document.createElement('tr');
+    for (let i = 0; i < llaves.length; i++) {
+        let thEl = document.createElement('th');
+        thEl.innerHTML = llaves[i];
+        trEl.appendChild(thEl);
+        thEl.classList.add("borders")
     }
-}
-let content = () => {
-    for (n in postres) {
-        let col1 = document.createElement('tr');
-        let col2 = document.createElement('td');
-        let col3 = document.createElement('td');
+    theadEl.appendChild(trEl);
+    tablaEl.appendChild(theadEl);
+};
+let filas = (elementos) => {
+    let trEl = document.createElement('tr');
+    let btn1 = document.createElement('button');
+    let btn2 = document.createElement('button');
+    btn1.innerText = "Editar";
+    btn2.innerText = "Eliminar";
+    btn1.classList.add("med");
+    btn2.classList.add("med");
+    for (i in elementos) {
+        let tdEl = document.createElement('input');
+        let j = document.createElement('td');
+        tdEl.value = elementos[i];
+        tdEl.readOnly = true;
+        tdEl.classList.add("borders");
+        j.appendChild(tdEl);
+        trEl.appendChild(j);
+        trEl.appendChild(btn1);
+        trEl.appendChild(btn2);
+        
+        btn1.addEventListener('click', () => {
+            trEl.classList.toggle("m");
+            trEl.classList.toggle("mem");
+            if (tdEl.readOnly == true) {
+                tdEl.readOnly = false;
+                btn1.innerText = "Listo";
+            } else if (tdEl.readOnly == false) {
+                tdEl.readOnly = true;
+                btn1.innerText = "Editar";
 
-        let btn1 = document.createElement('button');
-        let btn2 = document.createElement('button');
-        btn1.innerText = "Editar";
-        btn2.innerText = "Eliminar";
-
-        col1.innerText = postres[n].postre;
-        col1.classList.add("borders");
-        col2.innerText = postres[n].tiempoDePreparado;
-        col2.classList.add("borders");
-        col3.innerText = postres[n].cantidadDeIngredientes;
-        col3.classList.add("borders");
-
-        col1.appendChild(col2);
-        col1.appendChild(col3);
-        col1.appendChild(btn1);
-        col1.appendChild(btn2);
-        tablaBody.appendChild(col1);
-
-        btn2.addEventListener('click', () => {
-            tablaBody.removeChild(col1)
+            }
         })
     }
+    btn2.addEventListener('click', () => {
+        console.log("Boton eliminar");
+        console.log(trEl.rowIndex);
+        tBody.removeChild(trEl);
+    })
+    return trEl
 }
-let appenTable = () => {
-    tablaEl.appendChild(thead);
-    tablaEl.appendChild(tablaBody);
+
+let cuerpo = (elementos) => {
+    for (let i = 0; i < elementos.length; i++) {
+        tBody.appendChild(filas(elementos[i]));
+    }
+    tablaEl.appendChild(tBody);
+    tablaEl.classList.add("hide");
+};
+// GENERACION DE BOTONES MOSTRAR/OCULTAR TABLA Y AGREGAR
+let botton = () => {
+    btnAdd.classList.add("hide");
+    btnAdd.innerText = "Agregar";
+    btnShow.innerText = "Mostrar tabla";
+    modal.appendChild(btnShow);
+    modal.appendChild(btnAdd);
 }
+// FILA DE ELEMENTOS AGREGADOS
+let single = () => {
+    let trEl = document.createElement('tr');
+    let btn1 = document.createElement('button');
+    let btn2 = document.createElement('button');
+
+    btn1.innerText = "Editar";
+    btn2.innerText = "Eliminar";
+    btn1.classList.add("med");
+    btn2.classList.add("med");
+
+    for (i in arrayInput) {
+        let tdEl = document.createElement('input');
+        let j = document.createElement('td');
+        tdEl.value = arrayInput[i];
+        tdEl.readOnly = "readonly"
+        tdEl.classList.add("borders");
+        j.appendChild(tdEl);
+        trEl.appendChild(j);
+        trEl.appendChild(btn1);
+        trEl.appendChild(btn2);        
+        btn1.addEventListener('click', () => {
+            trEl.classList.toggle("m");
+            trEl.classList.toggle("mem");
+            if (tdEl.readOnly == true) {
+                tdEl.readOnly = false;
+                btn1.innerText = "Listo";
+            } else if (tdEl.readOnly == false) {
+                tdEl.readOnly = true;
+                btn1.innerText = "Editar";
+            }
+        })
+    }
+    btn2.addEventListener('click', () => {
+        console.log(trEl.rowIndex);
+        tBody.removeChild(trEl);
+    })
+    return trEl;
+}
+// MODAL QUE APARECE CUANDO PULSO EL BOTON "AGREGAR"
+let spam = () => {
+    inpuPostre.placeholder = llaves[0];
+    inpuTdp.placeholder = llaves[1];
+    inpuCdi.placeholder = llaves[2];
+    agreg.innerText = "Agregar";
+    cancelar.innerText = "Cancelar";
+    modalDivEl.appendChild(inpuPostre);
+    modalDivEl.appendChild(inpuTdp);
+    modalDivEl.appendChild(inpuCdi);
+    modalDivEl.appendChild(agreg);
+    modalDivEl.appendChild(cancelar);
+    modalDiv.appendChild(modalDivEl);
+    modalDivEl.classList.add("medio");
+}
+// ASIGNACION DE VALORES A LOS INPUTS, PEGADO DE LA NUEVA FILA AL BODY Y PUESTA DE CLASES
+let agregEventTasks = () => {
+    arrayInput[0] = inpuPostre.value;   
+    arrayInput[1] = inpuTdp.value;   
+    arrayInput[2] = inpuCdi.value;
+    tBody.appendChild(single(arrayInput));
+    modalDiv.classList.add("hide");
+    modalDiv.classList.remove("m");
+}
+// SE MUESTRA EN LA TABLA LA FILA AGREGADA Y SE VUELVEN LOS VALORES DE INPUTS A NULOS
+let agregEvent = () => {
+    agregEventTasks();
+    inpuPostre.value = "";
+    inpuTdp.value = "";
+    inpuCdi.value = "";
+}
+// SE MANIPULA LAS CLASES DEL DIV PARA QUE EL MODAL QUEDE VISIBLE
+let eventAdd = () => {
+    modalDiv.classList.remove("hide");
+    modalDiv.classList.add("m");
+}
+// EVENTO WINDOW, CUANDO CARGA LA PAGINA
 window.addEventListener('load', () => {
-    headeR();
-    content();
-    appenTable();
+    header(llaves);
+    cuerpo(elementos);
+    botton();
 })
+// BOTON MOSTRAR/OCULTAR TABLA
+btnShow.addEventListener('click', () => {
+    btnAdd.classList.toggle("hide");
+    tablaEl.classList.toggle("hide");
+    carrousel.classList.toggle("hide");
+    if (btnShow.innerText === "Mostrar tabla") {
+        btnShow.innerText = "Ocultar tabla";
+    } else if (btnShow.innerText === "Ocultar tabla") {
+        btnShow.innerText = "Mostrar tabla"
+    }
+})
+// BOTON AGREGAR
+btnAdd.addEventListener('click', () => {
+    spam();
+    eventAdd();
+})
+// BOTON QUE AGREGA LA FILA NUEVA A LA TABLA
+agreg.addEventListener('click', () => {
+    agregEvent();
+})
+// BOTON QUE CANCELA EL INPUT, VOLVIENDO A LA TABLA ORIGINAL
+cancelar.addEventListener('click', () => {
+    modalDiv.classList.add("hide");
+    modalDiv.classList.remove("m");
+    inpuPostre.value = "";
+    inpuTdp.value = "";
+    inpuCdi.value= "";
+})
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// MI IDEA ES HACER INPUTS EN CADA CELDA, PONERLES LOS VALORES Y DEJARLOS DESHABILITADOS
+// CUANDO TOQUE EDITAR SE VAN A HABILITAR Y LOS VOY A PODES MODIFICAR.
+
+
+// let single = () => {
+//     let trEl = document.createElement('tr');
+//     let j = document.createElement('td');
+//     let btn1 = document.createElement('button');
+//     let btn2 = document.createElement('button');
+
+//     btn1.innerText = "Editar";
+//     btn2.innerText = "Eliminar";
+
+//     // let tdPo = document.createElement('input');
+//     // let tdTi = document.createElement('input');
+//     // let tdCa = document.createElement('input');
+
+//     for (i in arrayInput) {
+//         let tdEl = document.createElement('input');
+//         let j = document.createElement('td');
+//         tdEl.value = arrayInput[i];
+//         tdEl.readOnly = "readonly"
+//         tdEl.classList.add("borders");
+//         j.appendChild(tdEl);
+//         trEl.appendChild(j);
+//         trEl.appendChild(btn1);
+//         trEl.appendChild(btn2);
+//     }
+
+//     // tdPo.innerHTML = arrayInput[0];
+//     // tdTi.innerHTML = arrayInput[1];
+//     // tdCa.innerHTML = arrayInput[2];
+
+//     // tdPo.classList.add("borders");
+//     // tdTi.classList.add("borders");
+//     // tdCa.classList.add("borders");
+
+//     // trEl.appendChild(tdPo); 
+//     // trEl.appendChild(tdTi); 
+//     // trEl.appendChild(tdCa); 
+//     trEl.appendChild(btn1);
+//     trEl.appendChild(btn2);
+//     btn1.addEventListener('click', () => {
+//         console.log("Boton editar");
+//         spam();
+//         eventAdd();
+
+//     })
+//     btn2.addEventListener('click', () => {
+//         console.log(trEl.rowIndex);
+//         tBody.removeChild(trEl);
+//     })
+//     return trEl;
+// }
+
+    // btn1.addEventListener('click', () => {
+    //     console.log("Boton editar");
+    //     console.log(trEl.rowIndex);
+    //     spamEdit();
+    //     eventAdd();
+    //     inpuPostre.value = elementos.postre;
+    //     inpuTdp.value = elementos.tiempoDePreparado;
+    //     inpuCdi.value= elementos.cantidadDeIngredientes;
+    // })
+
+    // MODAL QUE APARECE CUANDO PULSO EL BOTON "EDITAR"
+// let spamEdit = () => {
+//     inpuPostre.value = elementos.postre;
+//     inpuTdp.value = elementos.tiempoDePreparado;
+//     inpuCdi.value = elementos.cantidadDeIngredientes;
+//     editar.innerText = "Editar";
+//     cancelar.innerText = "Cancelar";
+//     modalEdit.appendChild(inpuPostre);
+//     modalEdit.appendChild(inpuTdp);
+//     modalEdit.appendChild(inpuCdi);
+//     modalEdit.appendChild(editar);
+//     modalEdit.appendChild(cancelar);
+//     modalDiv.appendChild(modalEdit);
+//     modalDiv.classList.add("hide");
+//     modalEdit.classList.add("medio");
+// }
